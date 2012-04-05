166d878a1012303fa0d5f3798888ece064bbb6bf
Víctor Fernández de Alba <victor.fernandez@upcnet.es>
2012-04-05 11:00:56 +0200

    Fix user none.

--- bigmax/utils.py
+++ bigmax/utils.py
@@ -5,5 +5,8 @@ def normalize_userdn(dn):
     """ Extract user id (e.g. cn=victor.fernandez,ou=Users,dc=upc,dc=edu to
         victor.fernandez, or leave username intact
     """
-    regex = r'(cn=)?([^,=]*),?'
-    return re.search(regex, dn).groups()[1]
+    if dn:
+        regex = r'(cn=)?([^,=]*),?'
+        return re.search(regex, dn).groups()[1]
+    else:
+        return None
