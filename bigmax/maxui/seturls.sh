e8b0d115dff2f1ea401766e1eff582efebb1e1ab
Carles Bruguera <carlesba@gmail.com>
2013-02-14 13:02:47 +0100

    New javascript with bugfixes, css fixes and comments on script

--- bigmax/maxui/seturls.sh
+++ bigmax/maxui/seturls.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+#
+# Sets the correct url for css resources, i and html images based on a url parameter
+# i.e. http://rocalcom.upc.edu (WITHOUT Trailing slash)
+# This has to be executed in a local branch in production with its correct url
+#
 sed -i "s/\(url('\/\)/url('https:\/\/$1\//g" maxui.css
 sed -i "s/\(src=\"\/\)/src=\"https:\/\/$1\//g" maxui.js
