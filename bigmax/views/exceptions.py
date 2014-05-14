74d507dc785082c7735e5eed31c8461b1b5ff9cb
Carles Bruguera <carlesba@gmail.com>
2014-05-14 16:14:58 +0200

    Fix exceptions view for domains

--- bigmax/views/exceptions.py
+++ bigmax/views/exceptions.py
@@ -10,15 +10,24 @@ from pygments.formatters import HtmlFormatter
 from DateTime import DateTime
 
 
-@view_config(context=MaxServer, route_name="exception", renderer='bigmax:templates/exceptions.pt', permission='restricted')
+@view_config(context=MaxServer, name="exceptions", renderer='bigmax:templates/exceptions.pt', permission='restricted')
 def configView(context, request):
-
-    exception_report = context.maxclient.admin.maintenance.exceptions[request.matchdict['id']].get()
     page_title = "BIG MAX Exception Log"
     api = TemplateAPI(context, request, page_title)
-    return dict(api=api,
+
+    if request.subpath:
+        exception_id = request.subpath[0]
+        try:
+            exception_report = context.maxclient.admin.maintenance.exceptions[exception_id].get()
+        except:
+            return dict(
+                api=api,
+                exception_id=None)
+        else:
+            return dict(
+                api=api,
                 date=DateTime(exception_report['date']).strftime('%Y/%M/%d %H:%M:%S'),
-                exception_id=request.matchdict['id'],
+                exception_id=exception_id,
                 http_request=highlight(exception_report['request'], HttpLexer(), HtmlFormatter(style='friendly')),
                 traceback=highlight(exception_report['traceback'], PythonTracebackLexer(), HtmlFormatter(style="friendly")),
                 url='%s/exeptions' % api.application_url)
