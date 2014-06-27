c29b136e64220f9b9bdf3bd267dac8e049a5dd78
Carles Bruguera <carlesba@gmail.com>
2014-06-27 09:10:17 +0200

    Back to development: 4.0.9

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ requires = [
 test_requires = ['WebTest', 'mock', ]
 
 setup(name='bigmax',
-      version='4.0.8',
+      version='4.0.9.dev0',
       description='Big MAX - MAX Web Administration Interface',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
